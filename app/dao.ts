f0841c700a78199e4a31276bb29f52ea808c0683
yossikolel <yossi.kolel@dh.com>
2016-07-26 11:02:09 +0300

    remove

--- app/dao.ts
+++
@@ -1,9 +0,0 @@
-import * as mongodb from 'mongodb-async-wrapper';
-
-export class Dao {
-
-    constructor(){
-        var db = mongodb.MongoClient.connect('mongodb://127.0.0.1');        
-    }
-
-}
